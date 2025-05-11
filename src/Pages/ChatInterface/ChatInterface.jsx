ca5526ff7b837c0b81c4ce8f18857fae078f726b
Sanjay S <sanjays13408@gmail.com>
2025-05-11 08:39:24 +0530

    updated

--- src/Pages/ChatInterface/ChatInterface.jsx
+++ src/Pages/ChatInterface/ChatInterface.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 import LawCharacter from "../../assets/images/LawCharacter.jpeg";
 
 const ChatInterface = () => {
-  const [userInput, setUserInput] = useState('');
+  const [userInput, setUserInput] = useState("");
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
@@ -23,14 +23,15 @@ const ChatInterface = () => {
     "தடை செய்யப்பட்ட பகுதிகளுக்கு செல்வதற்காக பொது இடத்தில் ராணுவ சீருடை அணிந்து, ராணுவ வீரர் போல் நடந்து பிடிபட்டனர்.",
     "அண்டை நாட்டின் எல்லையில் சட்டவிரோதமாக ஒரு நபர் திருடப்பட்ட சொத்துகளை வாங்குகிறார். IPC பிரிவு 127ன் படி என்ன விளைவுகள்?",
     "ஒரு பொது ஊழியர் தப்பிக்க அனுமதிக்கிறார். IPC பிரிவு 128ன் கீழ் அவர் எதிர்கொள்ளக்கூடிய தண்டனை என்ன?",
-    "தேசத்துரோகத்தை தூண்ட முயற்சித்ததற்காக, IPC பிரிவு 124A இன் கீழ் என்ன தண்டனைகளை எதிர்கொள்ளலாம்?"
+    "தேசத்துரோகத்தை தூண்ட முயற்சித்ததற்காக, IPC பிரிவு 124A இன் கீழ் என்ன தண்டனைகளை எதிர்கொள்ளலாம்?",
   ];
 
   useEffect(() => {
-    const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+    const SpeechRecognition =
+      window.SpeechRecognition || window.webkitSpeechRecognition;
     if (SpeechRecognition) {
       const recognition = new SpeechRecognition();
-      recognition.lang = 'ta-IN';
+      recognition.lang = "ta-IN";
       recognition.interimResults = false;
       recognition.maxAlternatives = 1;
 
@@ -45,16 +46,16 @@ const ChatInterface = () => {
 
       recognitionRef.current = recognition;
     } else {
-      console.error('Speech Recognition API not supported in this browser.');
+      console.error("Speech Recognition API not supported in this browser.");
     }
   }, []);
 
-  useEffect(() => {
-    const token = sessionStorage.getItem('token');
-    if (!token) {
-      navigate('/');
-    }
-  }, [navigate]);
+  // useEffect(() => {
+  //   const token = sessionStorage.getItem('token');
+  //   if (!token) {
+  //     navigate('/');
+  //   }
+  // }, [navigate]);
 
   const handleVoiceInput = () => {
     if (recognitionRef.current) {
@@ -74,35 +75,33 @@ const ChatInterface = () => {
     e.preventDefault();
 
     if (!userInput) {
-      setError('Please enter a message');
+      setError("Please enter a message");
       return;
     }
 
     setShowPrompts(false);
 
-    const userMessage = { sender: 'user', text: userInput };
+    const userMessage = { sender: "user", text: userInput };
     setMessages((prevMessages) => [...prevMessages, userMessage]);
-    setUserInput('');
-
+    setUserInput("");
     setLoading(true);
     setError(null);
 
     try {
-      const result = await axios.post('http://127.0.0.1:5000/chat', {
-        message: userMessage.text,
+      const result = await axios.post("http://127.0.0.1:5000/search", {
+        question: userMessage.text,
       });
+      console.log(result.data);
 
-      const botMessages = result.data.map(doc => ({
-        sender: 'bot',
-        text: doc.content,
-        title: doc.title,
-        section: doc.section,
-        punishment: doc.punishment,
-      }));
+      const botMessage = {
+        sender: "bot",
+        text: result.data.answer, // only "answer" key from response
+      };
 
-      setMessages((prevMessages) => [...prevMessages, ...botMessages]);
+      setMessages((prevMessages) => [...prevMessages, botMessage]);
     } catch (err) {
-      setError('Error fetching data from the chatbot.');
+      console.error(err);
+      setError("Error fetching data from the chatbot.");
     } finally {
       setLoading(false);
     }
@@ -119,25 +118,62 @@ const ChatInterface = () => {
   };
 
   const logOut = () => {
-    sessionStorage.removeItem('token');
-    navigate('/');
+    sessionStorage.removeItem("token");
+    navigate("/");
   };
 
   return (
     <div className="flex h-screen bg-gradient-to-r from-[#040404] to-[#2d8cff]">
       {/* Sidebar */}
-      <div className={`transition-all duration-300 bg-gray-800 p-2 pt-5 flex flex-col h-screen ${isSidebarExpanded ? 'w-1/4' : 'w-16'}`}>
-        <button onClick={toggleSidebar} className="text-gray-300 focus:outline-none">
-          <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24" className='border-2 bg-gray-700 border-gray-600 rounded-full p-2'>
-            <path fill="none" stroke="white" stroke-linecap="round" stroke-width="1.5" d="M20 7H4m16 5H4m16 5H4" />
+      <div
+        className={`transition-all duration-300 bg-gray-800 p-2 pt-5 flex flex-col h-screen ${
+          isSidebarExpanded ? "w-1/4" : "w-16"
+        }`}
+      >
+        <button
+          onClick={toggleSidebar}
+          className="text-gray-300 focus:outline-none"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="3em"
+            height="3em"
+            viewBox="0 0 24 24"
+            className="border-2 bg-gray-700 border-gray-600 rounded-full p-2"
+          >
+            <path
+              fill="none"
+              stroke="white"
+              stroke-linecap="round"
+              stroke-width="1.5"
+              d="M20 7H4m16 5H4m16 5H4"
+            />
           </svg>
         </button>
         <div className="">
-          <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24" className='mt-5 border-2 bg-gray-700 border-gray-600 rounded-full p-2'>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="3em"
+            height="3em"
+            viewBox="0 0 24 24"
+            className="mt-5 border-2 bg-gray-700 border-gray-600 rounded-full p-2"
+          >
             <path fill="white" d="M11 13H5v-2h6V5h2v6h6v2h-6v6h-2z" />
           </svg>
-          <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24" className='mt-[450px]'>
-            <g fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="3em"
+            height="3em"
+            viewBox="0 0 24 24"
+            className="mt-[450px]"
+          >
+            <g
+              fill="none"
+              stroke="white"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="1.5"
+            >
               <path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10s10-4.477 10-10S17.523 2 12 2" />
               <path d="M4.271 18.346S6.5 15.5 12 15.5s7.73 2.846 7.73 2.846M12 12a3 3 0 1 0 0-6a3 3 0 0 0 0 6" />
             </g>
@@ -148,27 +184,54 @@ const ChatInterface = () => {
       {/* Main Chat Area */}
       <div className="flex flex-col w-full bg-gray-900">
         {/* Chat Header */}
-        <div className='flex items-center justify-between m-5 font-outfit'>
-          <div className='flex items-center'>
-            <img src={LawCharacter} alt="" width="60px" height="60px" className='border-1 rounded-full'/>
-            <div className='ml-3'>
-              <p className='font-bold text-white'>Copsify AI</p>
-              <div className='flex items-center gap-2 mt-1'>
-                <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-                  <path fill="#34A853" d="M12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22" />
+        <div className="flex items-center justify-between m-5 font-outfit">
+          <div className="flex items-center">
+            <img
+              src={LawCharacter}
+              alt=""
+              width="60px"
+              height="60px"
+              className="border-1 rounded-full"
+            />
+            <div className="ml-3">
+              <p className="font-bold text-white">Copsify AI</p>
+              <div className="flex items-center gap-2 mt-1">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="1em"
+                  height="1em"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="#34A853"
+                    d="M12 22q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22"
+                  />
                 </svg>
-                <p className='text-gray-300'>Always active</p>
+                <p className="text-gray-300">Always active</p>
               </div>
             </div>
           </div>
-          <div style={{ position: 'relative', display: 'inline-block' }}>
-            <div onClick={toggleDropdown} style={{ cursor: 'pointer' }}>
-              <svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24">
-                <path fill="white" fillRule="evenodd" d="M12 4a8 8 0 0 0-6.96 11.947A4.99 4.99 0 0 1 9 14h6a4.99 4.99 0 0 1 3.96 1.947A8 8 0 0 0 12 4m7.943 14.076q.188-.245.36-.502A9.96 9.96 0 0 0 22 12c0-5.523-4.477-10-10-10S2 6.477 2 12a9.96 9.96 0 0 0 2.057 6.076l-.005.018l.355.413A9.98 9.98 0 0 0 12 22q.324 0 .644-.02a9.95 9.95 0 0 0 5.031-1.745a10 10 0 0 0 1.918-1.728l.355-.413zM12 6a3 3 0 1 0 0 6a3 3 0 0 0 0-6" clipRule="evenodd" />
+          <div style={{ position: "relative", display: "inline-block" }}>
+            <div onClick={toggleDropdown} style={{ cursor: "pointer" }}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="3em"
+                height="3em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="white"
+                  fillRule="evenodd"
+                  d="M12 4a8 8 0 0 0-6.96 11.947A4.99 4.99 0 0 1 9 14h6a4.99 4.99 0 0 1 3.96 1.947A8 8 0 0 0 12 4m7.943 14.076q.188-.245.36-.502A9.96 9.96 0 0 0 22 12c0-5.523-4.477-10-10-10S2 6.477 2 12a9.96 9.96 0 0 0 2.057 6.076l-.005.018l.355.413A9.98 9.98 0 0 0 12 22q.324 0 .644-.02a9.95 9.95 0 0 0 5.031-1.745a10 10 0 0 0 1.918-1.728l.355-.413zM12 6a3 3 0 1 0 0 6a3 3 0 0 0 0-6"
+                  clipRule="evenodd"
+                />
               </svg>
             </div>
             {isOpen && (
-              <div className="absolute top-14 right-0 bg-gray-800 shadow-lg p-2 px-3 rounded-md z-50" onClick={logOut}>
+              <div
+                className="absolute top-14 right-0 bg-gray-800 shadow-lg p-2 px-3 rounded-md z-50"
+                onClick={logOut}
+              >
                 <p className="m-0 cursor-pointer text-white">Logout</p>
               </div>
             )}
@@ -182,7 +245,9 @@ const ChatInterface = () => {
               <h1 className="md:text-5xl text-2xl font-semibold bg-gradient-to-r from-[#FF9090] to-[#8F0092] bg-clip-text text-transparent">
                 Hello, <span>Ragavan</span>
               </h1>
-              <p className="text-gray-400 mt-2 md:text-5xl text-2xl">How can I help you today?</p>
+              <p className="text-gray-400 mt-2 md:text-5xl text-2xl">
+                How can I help you today?
+              </p>
             </div>
             <div className="px-5 pt-5">
               <div className="grid md:grid-cols-4 grid-cols-2 gap-4">
@@ -192,7 +257,9 @@ const ChatInterface = () => {
                     className="border-2 border-gray-600 p-4 rounded-lg bg-gray-800 cursor-pointer"
                     onClick={() => handlePromptClick(prompt)}
                   >
-                    <p className="text-gray-300 md:text-base text-xs">{prompt}</p>
+                    <p className="text-gray-300 md:text-base text-xs">
+                      {prompt}
+                    </p>
                   </div>
                 ))}
               </div>
@@ -203,16 +270,20 @@ const ChatInterface = () => {
         {/* Messages */}
         <div ref={chatBoxRef} className="flex-grow p-5 overflow-y-auto">
           {messages.map((message, index) => (
-            <div key={index} className={`mb-4 ${message.sender === 'user' ? 'text-right' : 'text-left'}`}>
-              <div className={`inline-block p-4 rounded-lg ${message.sender === 'user' ? 'bg-blue-600 text-white' : 'bg-gray-700 text-gray-200'}`}>
+            <div
+              key={index}
+              className={`mb-4 ${
+                message.sender === "user" ? "text-right" : "text-left"
+              }`}
+            >
+              <div
+                className={`inline-block p-4 rounded-lg ${
+                  message.sender === "user"
+                    ? "bg-blue-600 text-white"
+                    : "bg-gray-700 text-gray-200"
+                }`}
+              >
                 <p>{message.text}</p>
-                {message.sender === 'bot' && (
-                  <>
-                    <p className="mt-2 text-sm"><span className='font-bold'>Description:</span> {message.title}</p>
-                    <p className="mt-2 text-sm"><span className='font-bold'>Section:</span> {message.section}</p>
-                    <p className="text-sm"><span className='font-bold'>Punishment:</span> {message.punishment}</p>
-                  </>
-                )}
               </div>
             </div>
           ))}
@@ -233,25 +304,53 @@ const ChatInterface = () => {
               onClick={handleVoiceInput}
               className="text-white p-3 rounded-full absolute right-20"
             >
-              <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" viewBox="0 0 24 24">
-                <path fill="#ffffff" d="M12 14q-1.25 0-2.125-.875T9 11V5q0-1.25.875-2.125T12 2t2.125.875T15 5v6q0 1.25-.875 2.125T12 14m-1 7v-3.075q-2.6-.35-4.3-2.325T5 11h2q0 2.075 1.463 3.538T12 16t3.538-1.463T17 11h2q0 2.625-1.7 4.6T13 17.925V21zm1-9q.425 0 .713-.288T13 11V5q0-.425-.288-.712T12 4t-.712.288T11 5v6q0 .425.288.713T12 12" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.5em"
+                height="1.5em"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="#ffffff"
+                  d="M12 14q-1.25 0-2.125-.875T9 11V5q0-1.25.875-2.125T12 2t2.125.875T15 5v6q0 1.25-.875 2.125T12 14m-1 7v-3.075q-2.6-.35-4.3-2.325T5 11h2q0 2.075 1.463 3.538T12 16t3.538-1.463T17 11h2q0 2.625-1.7 4.6T13 17.925V21zm1-9q.425 0 .713-.288T13 11V5q0-.425-.288-.712T12 4t-.712.288T11 5v6q0 .425.288.713T12 12"
+                />
               </svg>
             </button>
-            <button
-              type="submit"
-              className="ml-3"
-              disabled={loading}
-            >
+            <button type="submit" className="ml-3" disabled={loading}>
               {loading ? (
-                <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
-                  <path fill="#ffffff" d="M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z" opacity="0.5"/>
-                  <path fill="#ffffff" d="M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z">
-                    <animateTransform attributeName="transform" dur="1s" from="0 12 12" repeatCount="indefinite" to="360 12 12" type="rotate"/>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="2em"
+                  height="2em"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    fill="#ffffff"
+                    d="M12 2A10 10 0 1 0 22 12A10 10 0 0 0 12 2Zm0 18a8 8 0 1 1 8-8A8 8 0 0 1 12 20Z"
+                    opacity="0.5"
+                  />
+                  <path
+                    fill="#ffffff"
+                    d="M20 12h2A10 10 0 0 0 12 2V4A8 8 0 0 1 20 12Z"
+                  >
+                    <animateTransform
+                      attributeName="transform"
+                      dur="1s"
+                      from="0 12 12"
+                      repeatCount="indefinite"
+                      to="360 12 12"
+                      type="rotate"
+                    />
                   </path>
                 </svg>
               ) : (
-                <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
-                  <path fill="#3A4DE2" d="M3 20v-6l8-2l-8-2V4l19 8z"/>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="2em"
+                  height="2em"
+                  viewBox="0 0 24 24"
+                >
+                  <path fill="#3A4DE2" d="M3 20v-6l8-2l-8-2V4l19 8z" />
                 </svg>
               )}
             </button>
